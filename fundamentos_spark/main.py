d2b93bcc4596f10e8678b19897644d13ae9225ea
Mariangela Medeiros Crispim Araujo <mmcrispim@yahoo.com.br>
2024-11-19 00:10:45 +0000

    build: criando o projeto usando pyspark e docker para o curso fundamentos spark

--- fundamentos_spark/main.py
+++ fundamentos_spark/main.py
@@ -1,1 +1,25 @@
-print('Testando meu arquivo main.py')
+# import libraries & init spark session
+from pyspark.sql import SparkSession
+
+def main():
+    # Inicializa a sessão Spark
+    #spark = SparkSession.builder.appName("Basic Test").getOrCreate()
+    spark = SparkSession.builder \
+        .appName("PySpark Installation Test") \
+        .config("spark.eventLog.enabled", "false") \
+        .getOrCreate()
+
+    # Ajusta o nível de log para DEBUG
+    #spark.sparkContext.setLogLevel("DEBUG")
+    
+    # load data
+    print("Lendo o arquivo e criando o df_device")
+    df_device = spark.read.json("/workspace/Files/device_part_1.json")
+    print("Leu o arquivo e criou o df_device")
+    df_device.show()
+    
+    # Mensagem de validação
+    print("O processo foi executado com sucesso!")
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import unittest
+
+from fundamentos_spark.main import main
+
+
+class TestPySparkApp(unittest.TestCase):
+    def test_spark(self):
+        # Aqui você pode escrever testes para verificar seu código Spark
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
